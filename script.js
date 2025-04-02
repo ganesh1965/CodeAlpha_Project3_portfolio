b4e3c0d5f9ddd9c4309a8c102c25e8827ee174aa
ganesh1965 <166935099+ganesh1965@users.noreply.github.com>
2025-04-02 16:04:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-var imageThumbs = document.getElementById("image-thumbs");
-var currentImage = document.getElementById("current-image");
+ <script>
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".slide");
+    const sliderContainer = document.getElementById("slider-container");
 
-for (var i = 1; i <= 10; i++) {
-  var thumb = document.createElement("img");
-  thumb.src = "images/image" + i + ".jpg";
-  thumb.alt = "Image " + i;
-  thumb.classList.add("thumb");
-  imageThumbs.appendChild(thumb);
-  thumb.addEventListener(
-    "click", function() {
-      currentImage.src = this.src;
+    function openSlider(index) {
+      slideIndex = index;
+      sliderContainer.style.display = "flex";
+      updateSlide();
     }
-  );
-}
+
+    function closeSlider() {
+      sliderContainer.style.display = "none";
+    }
+
+    function changeSlide(step) {
+      slideIndex += step;
+      if (slideIndex < 0) slideIndex = slides.length - 1;
+      if (slideIndex >= slides.length) slideIndex = 0;
+      updateSlide();
+    }
+
+    function updateSlide() {
+      slides.forEach((slide, i) => {
+        slide.style.display = i === slideIndex ? "block" : "none";
+      });
+    }
+
+  </script>
